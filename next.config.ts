5c5df2e0cdeb7fb46f0c037f225a50bb0bcd7e38
jeremy@dubchuck.com <jeremy@dubchuck.com>
2025-12-03 06:02:45 -0800

    fix build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    // Disable Next.js optimizations for images (it's not available for static sites
+    // and will throw an error)
+    unoptimized: true,
+  },
+
+  // This tells Next.js to export a static build to the `out` folder
+  output: "export",
+
+  // This tells Next.js to export pages as "folders with an `index.html` file inside"
+  // We use this option so we can avoid having the `.html` extension at the end of the page URLs.
+  trailingSlash: true,
 };
 
 export default nextConfig;
