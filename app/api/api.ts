2912d9cf4dff180c9dab9ec081cc505339c13748
jeremy@dubchuck.com <jeremy@dubchuck.com>
2025-12-08 09:03:22 -0800

    add cache to axios calls

--- app/api/api.ts
+++ app/api/api.ts
@@ -1,4 +1,8 @@
-import axios from "axios";
+import Axios from "axios";
+import { setupCache } from 'axios-cache-interceptor';
+
+const instance = Axios.create();
+const axios = setupCache(instance);
 
 const BASE_URL = process.env.NEXT_PUBLIC_API_URL;
 
