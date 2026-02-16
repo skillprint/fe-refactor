6fea9f68ff42dd7d09cead1d95bef1da0fd2ce8c
jeremy@dubchuck.com <jeremy@dubchuck.com>
2026-02-16 10:37:29 -0800

    update loader

--- app/components/BuckyballLoading.tsx
+++ app/components/BuckyballLoading.tsx
@@ -3,18 +3,33 @@ import React from 'react';
 const BuckyballLoading = () => {
     return (
         <svg width="80" height="80" viewBox="0 0 50 50">
-            <circle cx="40" cy="25" r="4" fill="#05DF91">
-                <animate attributeName="r" values="4;6;4" dur="1.5s" begin="0s" repeatCount="indefinite"></animate>
-                <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0s" repeatCount="indefinite"></animate>
-            </circle>
-            <circle cx="17.500000000000004" cy="37.99038105676658" r="4" fill="#03ee9bff">
-                <animate attributeName="r" values="4;6;4" dur="1.5s" begin="0.2s" repeatCount="indefinite"></animate>
-                <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0.2s" repeatCount="indefinite"></animate>
-            </circle>
-            <circle cx="17.499999999999993" cy="12.009618943233423" r="4" fill="#C2EE14">
-                <animate attributeName="r" values="4;6;4" dur="1.5s" begin="0.4s" repeatCount="indefinite"></animate>
-                <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0.4s" repeatCount="indefinite"></animate>
-            </circle>
+            <g transform="translate(25 25)">
+                <g>
+                    <animateTransform
+                        attributeName="transform"
+                        type="rotate"
+                        values="0;120;120;240;240;360;360"
+                        keyTimes="0;0.05;0.333;0.383;0.666;0.716;1"
+                        dur="6s"
+                        repeatCount="indefinite"
+                    />
+                    <circle cx="10" cy="0" r="3" fill="#05DF91">
+                        <animate attributeName="r" values="3;4.5;3" dur="1.5s" begin="0s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill" values="#05DF91;#0591DF;#DEDE05;#05DF91" dur="6s" repeatCount="indefinite"></animate>
+                    </circle>
+                    <circle cx="-5" cy="8.66" r="3" fill="#05DF91">
+                        <animate attributeName="r" values="3;4.5;3" dur="1.5s" begin="0.2s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0.2s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill" values="#05DF91;#0591DF;#DEDE05;#05DF91" dur="6s" repeatCount="indefinite"></animate>
+                    </circle>
+                    <circle cx="-5" cy="-8.66" r="3" fill="#05DF91">
+                        <animate attributeName="r" values="3;4.5;3" dur="1.5s" begin="0.4s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill-opacity" values="1;0.5;1" dur="1.5s" begin="0.4s" repeatCount="indefinite"></animate>
+                        <animate attributeName="fill" values="#05DF91;#0591DF;#DEDE05;#05DF91" dur="6s" repeatCount="indefinite"></animate>
+                    </circle>
+                </g>
+            </g>
         </svg>
     );
 };
